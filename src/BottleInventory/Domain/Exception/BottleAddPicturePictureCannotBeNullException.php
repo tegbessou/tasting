7c0d111ada418f764a11a92e3359418eb802e2e0
Tegbessou <hugues.gobet@gmail.com>
2024-12-17 15:22:02 +0100

    Remove bottle inventory bounded context

--- src/BottleInventory/Domain/Exception/BottleAddPicturePictureCannotBeNullException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace App\BottleInventory\Domain\Exception;
-
-final class BottleAddPicturePictureCannotBeNullException extends \InvalidArgumentException
-{
-}
